f03030611ffe8c4070a668db425cdf904c4cc077
Ankit Verma <ankitverma_0810@yahoo.co.in>
2021-04-07 16:45:09 +0530

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,10 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
 import { NotAuthorizedError } from '../errors/not-authorized-error';
-import { UserStatus } from '../events/types/user-status';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    if (!req.currentUser || req.currentUser.status === UserStatus.Inactive) {
+    if (!req.currentUser) {
         throw new NotAuthorizedError();
     }
     next();
